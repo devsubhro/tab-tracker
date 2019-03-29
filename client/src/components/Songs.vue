197b8f8eba734fb78bcccfe637a3d5aa6f5c8b42
SNG <sng@unifiedinfotech.net>
2019-03-29 15:25:17 +0530

    support for embedded view element using component

---
+++ client/src/components/Songs.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-layout column>
+    <v-flex xs6 offset-xs3 style="margin-right:25%">
+        <content-panel title="Songs">
+            <slot>
+                <!-- write the content that will appear in the <slot></slot> in the 
+                ContentPanel -->
+                <p>Well</p>
+            </slot>
+        </content-panel>
+    </v-flex>
+</v-layout>
+</template>
+
+<script>
+import ContentPanel from '@/components/ContentPanel'
+export default {
+    components: {
+        ContentPanel
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
+
